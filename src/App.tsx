6094d892516f76b5c0946ff3d0d6395ce11653c1
Nechamao <nechama.ozery@gmail.com>
2024-03-31 11:35:05 +0300

    add multi lang, grid lagout, dark mode

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
-import Demo from './Demo'
+import Demo from './components/Demo'
+import ExampleGrid from './components/ExampleGrid'
+import { ChangeLang } from './components/ChangeLang'
+import  {useTranslation} from 'react-i18next';
+
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const {t} = useTranslation();
 
   return (
     <>
          <Demo />
+         <ExampleGrid />
+         <ChangeLang />
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+      <h1>{t("title")}</h1>
+       
       </div>
  
     </>
